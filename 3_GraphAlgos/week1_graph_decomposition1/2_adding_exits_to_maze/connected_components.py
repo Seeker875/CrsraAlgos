44a2227c89d260c570ceaef6ba1120b9436b093e
Taranpreet singh <taran8727@gmail.com>
2020-08-10 15:23:38 -0400

    week1 materials

--- 3_GraphAlgos/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
+++ 3_GraphAlgos/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -2,10 +2,30 @@
 
 import sys
 
+def explore(v):
+    visited[v]=1
+   # print(adj[v])
+    for w in adj[v]:
+    #    print(f'w outside if is {w}')
+        if visited[w]==0:
+     #       print(f'w is {w}')
+            explore(w)
+    return 0
 
+def DFS(adj):
+    '''
+    Depth First search
+    '''
+    cc=0
+    for i in range(n):
+        if visited[i]==0:
+            explore(i)
+            cc+=1
+    return cc        
 def number_of_components(adj):
-    result = 0
+    result = DFS(adj)
     #write your code here
+    
     return result
 
 if __name__ == '__main__':
@@ -15,6 +35,7 @@ if __name__ == '__main__':
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     adj = [[] for _ in range(n)]
+    visited=[0]*n
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
